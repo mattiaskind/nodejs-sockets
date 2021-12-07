3c64ac04242e6281aa03ac0fa7e46b5c8e05020e
Mattias Kind <mattias.kind@gmail.com>
2021-12-07 08:57:10 +0100

    Funktioner som visar användare och när någon avslutar eller ändrar namn

--- index.js
+++ index.js
@@ -4,21 +4,42 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  console.log('En användare anslöt');
+let users = {};
 
-  socket.on('chat-message', (msg) => {
-    console.log(msg);
-    io.emit('chat-message', msg);
+const guest = 'Gäst';
+
+io.on('connection', (socket) => {
+  console.log(users);
+
+  // Körs vid ny anslutning
+  //users[socket.id] = 'Guest';
+
+  users[socket.id] = guest;
+  io.emit('updateUsersList', users);
+
+  socket.on('chat-message', (msgData) => {
+    if (users[socket.id.toLocaleLowerCase] === guest) {
+      io.emit('change', `${guest} byter namn till ${msgData.name}`);
+    } else if (users[socket.id] !== msgData.name) {
+      io.emit('change', `${users[socket.id]} byter namn till ${msgData.name}`);
+      users[socket.id] = msgData.name;
+      io.emit('updateUsersList', users);
+    }
+    io.emit('chat-message', msgData);
   });
 
   socket.on('disconnect', () => {
-    console.log('En användare avslutade');
+    delete users[socket.id];
+    io.emit('updateUsersList', users);
+    //users = users.filter((user) => user.id !== socket.id);
   });
 });
 
